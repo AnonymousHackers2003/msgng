46a708a4fad8e093b8b3da7fbd270ecf73945387
AnonymousHackers2003 <thetrueanonymous2003@gmail.com>
2025-06-19 06:43:21 +0300

    Create messages.js

---
+++ netlify/functions/messages.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async () => {
+  const filePath = path.join(__dirname, "../../data/messages.json");
+  const messages = JSON.parse(fs.readFileSync(filePath));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(messages)
+  };
+};
